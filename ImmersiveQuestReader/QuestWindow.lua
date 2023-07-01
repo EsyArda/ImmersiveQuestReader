ba940ae2bff2494ccb4adf1b1a89b3de00799e4d
Lilian Hiault <lilian.hiault@laposte.net>
2023-07-01 18:55:46 +0200

    Clic on the text to display the next paragraph. Replace the name of the player in the text

--- ImmersiveQuestReader/QuestWindow.lua
+++ ImmersiveQuestReader/QuestWindow.lua
@@ -1,6 +1,7 @@
 -- Quest Window
 
 import "Turbine.UI.Lotro";
+import "Turbine.Gameplay";
 
 QuestWindow = class(Turbine.UI.Lotro.Window);
 
@@ -16,6 +17,7 @@ function QuestWindow:Constructor()
     local x = (screenWidth - windowWidth) / 2;
     local y = (screenHeight - windowHeight) / 2;
     self:SetPosition(x, y);
+    self:SetMouseVisible(true);
     self:SetText("Quest name");
     self:SetVisible(false); -- Hidden by default
 
@@ -28,8 +30,29 @@ function QuestWindow:Constructor()
     self.questTextLabel:SetPosition(20, 40);
     self.questTextLabel:SetFont(Turbine.UI.Lotro.Font.BookAntiqua24);
     self.questTextLabel:SetText("Quest text");
-    self.questTextLabel:SetBackColor(Turbine.UI.Color(0,0.17,0.11));
     self.questTextLabel:SetVisible(true);
+    
+
+    -- Player (needed for their name)
+    self.player = Turbine.Gameplay.LocalPlayer:GetInstance();
+    
+    -- Current quest displayed
+    self.quest = nil;
+    -- Table of lines of the quest text
+    self.questLines = {};
+    -- Current index in the table of lines
+    self.currentLine = 1;
+    
+    
+    -- Can click on the text
+    self.questTextLabel.MouseUp = function(sender, args)
+        if self.currentLine < #self.questLines then
+            self.currentLine = self.currentLine + 1;
+            self:SetQuestText(self.questLines[self.currentLine]);
+        else
+            self:SetVisible(false);
+        end    
+    end
 end
 
 function QuestWindow:SetQuestName(questName)
@@ -41,13 +64,29 @@ function QuestWindow:SetQuestText(questText)
 end
 
 function QuestWindow:NewQuest(quest)
+    self.quest = quest;
     self:SetVisible(true);
     self:SetQuestName(quest.name);
-    local questText = quest.bestower[1].text;
-    -- Split the text in a table of lines
-    local lines = {};
-    for line in string.gmatch(questText, "[^\r\n]+") do
-        table.insert(lines, line);
+    
+    local questText = "";
+    if quest.bestower.text ~= nil and type(quest.bestower.text) == "string" then
+        questText = quest.bestower.text;
+    else
+        questText = quest.bestower[1].text;
     end
-    self:SetQuestText(quest.bestower[1].text);
+    questText = self:ComputeQuestText(questText);
+
+    -- Split the text in a table of lines
+    for line in string.gmatch(questText, "[^\n]+") do -- Match all characters except new line
+        table.insert(self.questLines, line);
+    end
+
+    self:SetQuestText(self.questLines[1]);
+end
+
+
+function QuestWindow:ComputeQuestText(questText)
+    local playerName = self.player:GetName();
+    local questTextComputed = string.gsub(questText, "${PLAYER}", playerName);
+    return questTextComputed;
 end
