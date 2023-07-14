85d3f27aab660bfd6ff2569f1144752a6ef3b67f
Lilian Hiault <lilian.hiault@laposte.net>
2023-07-14 18:08:15 +0200

    Load all quests databases and search in all of them

--- ImmersiveQuestReader/QuestManager.lua
+++ ImmersiveQuestReader/QuestManager.lua
@@ -1,11 +1,29 @@
 -- Quest Manager
 
-import "EsyIQR.ImmersiveQuestReader.QuestDatabase"
+import "EsyIQR.ImmersiveQuestReader.QuestDatabase0"
+import "EsyIQR.ImmersiveQuestReader.QuestDatabase1"
+import "EsyIQR.ImmersiveQuestReader.QuestDatabase2"
+import "EsyIQR.ImmersiveQuestReader.QuestDatabase3"
+import "EsyIQR.ImmersiveQuestReader.QuestDatabase4"
+import "EsyIQR.ImmersiveQuestReader.QuestDatabase5"
+import "EsyIQR.ImmersiveQuestReader.QuestDatabase6"
+import "EsyIQR.ImmersiveQuestReader.QuestDatabase7"
+import "EsyIQR.ImmersiveQuestReader.QuestDatabase8"
+import "EsyIQR.ImmersiveQuestReader.QuestDatabase9"
+import "EsyIQR.ImmersiveQuestReader.QuestDatabase10"
+import "EsyIQR.ImmersiveQuestReader.QuestDatabase11"
+import "EsyIQR.ImmersiveQuestReader.QuestDatabase12"
+import "EsyIQR.ImmersiveQuestReader.QuestDatabase13"
+import "EsyIQR.ImmersiveQuestReader.QuestDatabase14"
+import "EsyIQR.ImmersiveQuestReader.QuestDatabasem1"
+
 
 QuestManager = class();
 
 function QuestManager:Constructor()
-    self.quests = QUESTS.quest
+    if QUESTS then self.questsByLevel = QUESTS.quest end
+    self.questsByLevel = { QUESTS1.quest, QUESTS2.quest, QUESTS3.quest, QUESTS4.quest, QUESTS5.quest, QUESTS6.quest, QUESTS7.quest, QUESTS8.quest, QUESTS9.quest, QUESTS10.quest, QUESTS11.quest, QUESTS12.quest, QUESTS13.quest, QUESTS14.quest, QUESTSm1.quest}
+
 end
 
 function QuestManager:IsNewQuest(chatMessage)
@@ -35,10 +53,13 @@ end
 
 -- Returns the quest text for a given quest name
 function QuestManager:GetQuestTextFromName(questName)
-    for _, quest in pairs(self.quests) do
-        if quest.name == questName then
-            if DEBUG then Turbine.Shell.WriteLine("IQR.QuestManager> Quest found: '" .. quest.name .. "'") end
-            return quest -- Retourne la quête si le nom correspond
+    for _, database in pairs(self.questsByLevel) do
+        for _, quest in pairs(database) do
+            -- if quest.name then Turbine.Shell.WriteLine("IQR.QuestManager> Quest searched: '" .. quest.name .. "'") end
+            if quest.name == questName then
+                if DEBUG then Turbine.Shell.WriteLine("IQR.QuestManager> Quest found: '" .. quest.name .. "'") end
+                return quest -- Retourne la quête si le nom correspond
+            end
         end
     end
     return nil -- Retourne nil si la quête n'est pas trouvée
